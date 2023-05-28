7148570bd61c7370bed7beacbdd278502674bfbf
dedee42004 <120428440+dedee42004@users.noreply.github.com>
2023-05-28 22:54:41 +0300

    Update Example3.cpp

--- OOPLab6T/Example3.cpp
+++ OOPLab6T/Example3.cpp
@@ -1,95 +1,145 @@
-﻿#include "Lab6Example.h"
-#include <iostream>
-#include <string>
-using namespace std;
-namespace SpaceExample3 {
-    // MultipleiInherance.cpp 
-// Ієрархія типів складається з сутностей: 
-// машина, пасажирський транспорт і автобус.
-//
+class Transport {
+	public:
+		virtual void Ryx() = 0;
+	};
+
+	class Korabel :virtual public Transport {
+	public:
+		void Ryx() override
+		{
+			cout << "Tyyy TYYYyyy!" << endl;
+		}
+	};
+
+	class Pass_Transport : virtual public Transport {
+	public:
+		void People()
+		{
+			cout << "Bla Bla Bla!" << endl;
+		}
+		void Ryx() override
+		{
+			cout << "Bi Biip!" << endl;
+		}
+	};
+
+	class Pass_Korabel : public Korabel, public Pass_Transport
+	{
+	public:
+		void Music()
+		{
+			cout << "Bymc BymC Bymc!" << endl;
+		}
+		void Ryx() override
+		{
+			cout << "Tyyy Bymcc TYYYyyy TyyyYy!" << endl;
+		}
+	};
+
+	class Shym
+	{
+	public:
+		void Ryx(Transport* t)
+		{
+			t->Ryx();
+		}
+	};
+	int main()
+	{
+		cout << "OOP. Example for laboratory work #6.\n";
+
+		char ch = '5';
+		do {
+			system("cls");
+			MenuExample();
+			ch = cin.get();
+
+			cin.get();
+
+			switch (ch) {
+			case '1': {
+				cout << " Example1  \n";
+				R1 a1, b1(1, 2, 3, 4.5, 52);
+				R2 a2, b2(1, 2, 3, 4.5, 5, 6, 7, 8, 9, 10);
+				R1V a1v, b1v(1, 2, 3, 4.5, 52);
+				R2V a2v, b2v(1, 2, 3, 4.5, 5, 6, 7, 8, 9, 10);
+
+				cout << "Test !\n";
+				cout << "Size for Base " << sizeof(Base) << endl;
+				cout << "Size for D " << sizeof(D) << endl;
+				cout << "Size for D1 " << sizeof(D1) << endl;
+				cout << "Size for D2 " << sizeof(D2) << endl;
+				cout << "Size for BD " << sizeof(BD) << endl;
+				cout << "Size for R1 " << sizeof(R1) << endl;
+				cout << "Size for R2 " << sizeof(R2) << endl;
+				cout << "\n" << endl;
+				cout << "Size for Base " << sizeof(Base) << endl;
+				cout << "Size for DV " << sizeof(DV) << endl;
+				cout << "Size for D1V " << sizeof(D1V) << endl;
+				cout << "Size for D2V " << sizeof(D2V) << endl;
+				cout << "Size for BDV " << sizeof(BDV) << endl;
+				cout << "Size for R1V " << sizeof(R1V) << endl;
+				cout << "Size for R2V " << sizeof(R2V) << endl;
+
+				cout << "Size for object class R1 " << sizeof(R1) << " or  "
+					<< sizeof(a1) << " or  " << sizeof(b1) << endl;
+				cout << "Size for object class R2 " << sizeof(R2) << " or  "
+					<< sizeof(a2) << " or  " << sizeof(b2) << endl;
+				cout << "Size for object class R1V " << sizeof(R1V) << " or  "
+					<< sizeof(a1v) << " or  " << sizeof(b1v) << endl;
+				cout << "Size for object class R2V " << sizeof(R2V) << " or  "
+					<< sizeof(a2v) << " or  " << sizeof(b2v) << endl;
+				b1v.showDat();
+				b2v.showDat();
+				return 0;
+				break;
+			}
+			case '2': {
+				cout << " Example2  \n";
+				Rozv rivnyan;
+				cout << "Test Linear equation" << endl;
+				Lin La(2, 14);
+				rivnyan.Rivnya(&La);
+				cout << "Constructor ()" << endl;
+				Lin Lb;
+				rivnyan.Rivnya(&Lb);
+				cout << "Test Quadratic equation" << endl;
+				Kvad Kc(2, 9, -18);
+				rivnyan.Rivnya(&Kc);
+				Kvad Ke(2, -5, -25);
+				rivnyan.Rivnya(&Ke);
+				Kvad Kj(1, -1, 1);
+				rivnyan.Rivnya(&Kj);
+				cout << "Constructor ()" << endl;
+				Kvad Kd;
+				rivnyan.Rivnya(&Kd);
+				cout << "Test BiQuadratic equation" << endl;
+				Bikvad Bt(1, 1, -30);
+				rivnyan.Rivnya(&Bt);
+				cout << "Constructor ()" << endl;
+				Bikvad By;
+				rivnyan.Rivnya(&By);
+				break;
+			}
+			case '3': {
+				Shym f;
+				Korabel kor;
+				Pass_Korabel pkor;
+				Pass_Transport pt;
+				f.Ryx(&kor);
+				f.Ryx(&pkor);
+				pkor.People();
+				f.Ryx(&pt);
+				pt.People();
+
+				break;
+			}
+			case '4':  return 0;
+			case 'e':  return 0;
+			}
+			cout << " Press any key and enter\n";
+			ch = cin.get();
+		} while (ch != '6');
 
 
-    class Car {
-    protected:
-        string marka;
-        float power;
-        int numberOfWheels;
-    public:
-        Car() : marka("Neoplan"), power(5.2f), numberOfWheels(6) {
-        }
-        Car(string m, float p, int nw) : marka(m), power(p), numberOfWheels(nw) {
-        }
-        string getMarka() { return marka; }
-        void setMarka(string m) { marka = m; }
-        float getPower() {
-            return power;
-        }
-        void setPower(float p) {
-            power = p;
-        }
-        int getNumberOfWheels() {
-            return numberOfWheels;
-        }
-        void setNumberOfWheels(int n) {
-            numberOfWheels = n;
-        }
-        string toString() {
-            string r = marka + "\t" + to_string(power) + "\t" + to_string(numberOfWheels) + "\t";
-            return r;
-        }
-    };
-
-    class PassengerTransport {
-    protected:
-        int flightNumber;
-        int numberOfPassengerSeats;
-    public:
-        PassengerTransport() : flightNumber(101), numberOfPassengerSeats(45) {}
-        PassengerTransport(int f, int n) : flightNumber(f), numberOfPassengerSeats(n) {}
-        int  getFlightNumber() { return flightNumber; }
-        void setFlightNumber(int f) { flightNumber = f; }
-        int getNumberOfPassengerSeats() { return numberOfPassengerSeats; }
-        void setnumberOfPassengerSeats(int n) { numberOfPassengerSeats = n; }
-        string toString() {
-            string r = to_string(flightNumber) + "\t" + to_string(numberOfPassengerSeats) + "\t";
-            return r;
-        }
-    };
-    class AutoBus : public Car, public  PassengerTransport
-    {
-        string busRoute;
-    public:
-        AutoBus() : busRoute("Kyiv-Chernivci") {}
-        AutoBus(string m, float p, int nw, int f, int n, string bs)
-            : Car(m, p, nw), PassengerTransport(f, n), busRoute(bs) {}
-        string getbusRoute() {
-            return busRoute;
-        }
-        void setbusRoute(string bs) { busRoute = bs; }
-
-        string toString() {
-            string r = Car::toString() + PassengerTransport::toString() + busRoute;
-            return r;
-        }
-    };
-
-    int mainExample3()
-    {
-        AutoBus def;
-        AutoBus lvCh("Iveko", 6.2f, 6, 301, 40, "Lviv-Chernivci");
-        AutoBus* pVnCn = new AutoBus();
-        pVnCn->setMarka("Ikarus");
-        pVnCn->setPower(7.2f);
-        pVnCn->setNumberOfWheels(6);
-        pVnCn->setFlightNumber(403);
-        pVnCn->setnumberOfPassengerSeats(42);
-        pVnCn->setbusRoute("Vinnicya-Chernivci");
-
-        cout << def.toString() << endl;
-        cout << lvCh.toString() << endl;
-        cout << pVnCn->toString() << endl;
-        return 0;
-    }
-
-
-}
+	}
